ea0dd354e76db7726e346f2f32edf3d7dc4e31d6
Sergio Tulentsev <sergei.tulentsev@gmail.com>
2020-06-17 11:37:57 +0100

    CustomerFormatter: prepares customer for output

---
+++ lib/customer_formatter.rb
@@ -0,0 +1,20 @@
+require 'json'
+
+class CustomerFormatter
+  # @param writer [ #<< ] an IO-like object
+  def initialize(writer:)
+    @writer = writer
+  end
+
+  def <<(customer)
+    selected_attributes = {
+        user_id: customer.user_id,
+        name: customer.name
+    }
+    writer << selected_attributes.to_json
+  end
+
+  private
+
+  attr_reader :writer
+end
---
+++ spec/lib/customer_formatter_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+RSpec.describe CustomerFormatter do
+  let(:writer) { [] }
+  subject(:customer_formatter) { CustomerFormatter.new(writer: writer) }
+
+  describe '#<<' do
+    it 'outputs only user_id and name' do
+      customer = Customer.new(
+          user_id: 1,
+          name: 'Joe Smith',
+          position: GeoPosition.new(latitude: 0, longitude: 0),
+      )
+      initial = []
+      expected = ['{"user_id":1,"name":"Joe Smith"}']
+
+      expect {
+        customer_formatter << customer
+      }.to change { writer }.from(initial).to(expected)
+    end
+  end
+end
